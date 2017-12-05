a1ff74702255e46f90289d0d58c9cb20cb948759
Faisal <Faisal@SC-VAIO>
2017-12-05 23:54:41 +0100

    Hello GIT Home

---
+++ FSL-git/src/fsl/git/FSLGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fsl.git;
+
+/**
+ *
+ * @author Faisal
+ */
+public class FSLGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello GIT Home repo");
+    // TODO code application logic here
+    }
+    
+}
