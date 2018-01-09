dbec1626337c712004169dc4308f7fc3bab0150e
2701faiqbal <2701faiqbal@PC9782.domepfc.net>
2018-01-09 20:14:36 +0100

    rectangle

---
+++ NetBeansProjects/Temprature convert/src/Rectangle.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 2701faiqbal
+ */
+public class Rectangle {
+        // Data Variables
+        double height = 10;
+        double width = 10;
+        // Rectangle calcul
+        double getArea() {
+            return height * width;
+        }
+        // Perimeter calcul
+        double getPerimeter() {
+            return 2 + (height + width);
+        }
+        // 
+        public static void main(String[] args) {
+        //
+        //Rectangle.main(args);
+        //double number = Math.cos(Math.PI);
+            Rectangle r1 = new Rectangle();
+            r1.height = 10;
+            r1.width = 10;
+            System.out.println(r1.height);
+            System.out.println(r1.width);
+            
+    } 
+}
